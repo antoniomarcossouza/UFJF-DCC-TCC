f57f4e259578f174c3e2aac2ca67e87fe10f2058
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2025-08-30 13:58:26 -0300

    feature: salvando .xls localmente

--- execucao_orcamentaria/defs/pjf/assets/receitas.py
+++ execucao_orcamentaria/defs/pjf/assets/receitas.py
@@ -1,12 +1,9 @@
 from datetime import datetime
-from io import BytesIO
 
 import dagster as dg
-import pandas as pd
+from pathlib import Path
 import requests
 from dagster.components import definitions
-from dagster_duckdb import DuckDBResource
-from duckdb import DuckDBPyConnection
 
 
 def fetch_xls(url: str) -> bytes:
@@ -16,38 +13,21 @@ def fetch_xls(url: str) -> bytes:
     return response.content
 
 
-def parse_receita_excel(content: bytes, source_url: str) -> pd.DataFrame:
-    """Transforma bytes de XLS em DataFrame com colunas extras."""
-    df = pd.read_excel(BytesIO(content), skiprows=2)
-    df["filename"] = source_url.split("/")[-1]
-    df["imported_at"] = pd.Timestamp.utcnow()
-    return df
+class LocalFSResource(dg.ConfigurableResource):
+    base_path: str
 
+    def save_bytes(
+        self,
+        content: bytes,
+        directory: str,
+        filename: str,
+    ) -> None:
+        """Salva bytes no filesystem local."""
+        filepath = Path(self.base_path) / directory / filename
+        filepath.parent.mkdir(exist_ok=True, parents=True)
 
-def ensure_table_exists(
-    conn: DuckDBPyConnection,
-    df: pd.DataFrame,
-    schema: str,
-    table: str,
-) -> None:
-    """Cria a tabela staging se ainda não existir."""
-    conn.execute(f"create schema if not exists {schema}")
-    conn.execute(
-        f"""
-        create table if not exists {schema}.{table} as
-        select * from df limit 0
-        """
-    )
-
-
-def save_dataframe_to_duckdb(
-    conn: DuckDBPyConnection,
-    df: pd.DataFrame,
-    schema: str,
-    table: str,
-) -> None:
-    """Insere DataFrame em uma tabela existente."""
-    conn.execute(f"insert into {schema}.{table} select * from df")
+        with filepath.open("wb") as f:
+            f.write(content)
 
 
 receita_mensal_prevista_partition = dg.TimeWindowPartitionsDefinition(
@@ -55,6 +35,12 @@ receita_mensal_prevista_partition = dg.TimeWindowPartitionsDefinition(
     cron_schedule="0 0 1 1 *",
     fmt="%y",
     end_offset=1,
+)
+
+receita_mensal_comparativa_partition = dg.TimeWindowPartitionsDefinition(
+    start=datetime(2020, 1, 1),
+    cron_schedule="0 0 1 * *",
+    fmt="%y%m",
 )
 
 
@@ -66,38 +52,61 @@ receita_mensal_prevista_partition = dg.TimeWindowPartitionsDefinition(
 )
 def receita_mensal_prevista(
     context: dg.AssetExecutionContext,
-    duckdb: DuckDBResource,
+    fs: LocalFSResource,
 ) -> dg.MaterializeResult:
     """
     Baixa o XLS de receita mensal prevista,
     lê com pandas e salva em uma tabela DuckDB.
     Suporta backfill anual baseado em partições.
     """
-    ano = int(context.partition_key)
+    year = int(context.partition_key)
 
-    url = f"https://www.pjf.mg.gov.br/transparencia/receitas/mensal/previsao/arquivos/xls/{ano}.xls"
+    url = f"https://www.pjf.mg.gov.br/transparencia/receitas/mensal/previsao/arquivos/xls/{year}.xls"
     content = fetch_xls(url)
-    df = parse_receita_excel(content, url)
 
-    with duckdb.get_connection() as conn:
-        context.log.info("Teste")
+    fs.save_bytes(
+        content=content,
+        directory="pjf_receita_mensal_prevista",
+        filename=url.split("/")[-1],
+    )
 
-        ensure_table_exists(
-            conn=conn,
-            df=df,
-            schema="stg",
-            table="pjf_receita_mensal_prevista",
-        )
-        save_dataframe_to_duckdb(
-            conn=conn,
-            df=df,
-            schema="stg",
-            table="pjf_receita_mensal_prevista",
-        )
+    return dg.MaterializeResult()
+
+
+@dg.asset(
+    partitions_def=receita_mensal_comparativa_partition,
+    kinds={"python", "pandas", "duckdb"},
+    group_name="receitas",
+    description="Receita Mensal Comparativa",
+)
+def receita_mensal_comparativa(
+    context: dg.AssetExecutionContext,
+    fs: LocalFSResource,
+) -> dg.MaterializeResult:
+    year_month = context.partition_key
+
+    url = f"https://www.pjf.mg.gov.br/transparencia/receitas/mensal/comparativo/arquivos/xls/{year_month}.xls"
+    content = fetch_xls(url)
+
+    fs.save_bytes(
+        content=content,
+        directory="pjf_receita_mensal_comparativa",
+        filename=url.split("/")[-1],
+    )
 
     return dg.MaterializeResult()
 
 
 @definitions
 def defs():
-    return dg.Definitions(assets=[receita_mensal_prevista])
+    return dg.Definitions(
+        assets=[
+            receita_mensal_prevista,
+            receita_mensal_comparativa,
+        ],
+        resources={
+            "fs": LocalFSResource(
+                base_path="/home/antonio/projects/ufjf/ufjf-tcc/data"
+            )
+        },
+    )
