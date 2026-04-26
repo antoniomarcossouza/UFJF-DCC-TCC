3afe85855267774a3b6d700b03b387f71a1dea4b
Antônio Marcos <antoniomarcos.souza2002@gmail.com>
2026-04-25 22:51:11 -0300

    feat: nome das colunas

--- execucao_orcamentaria_dbt/models/warehouse/pjf/fct_receita_comparativa.sql
+++ execucao_orcamentaria_dbt/models/warehouse/pjf/fct_receita_comparativa.sql
@@ -1,9 +1,11 @@
 select
-    "Previsão Inicial",
-    "Previsão Atualizada",
-    "Arrecadada Mês",
-    "Arrecadada Ano",
-    "A Realizar",
-    "Natureza",
-    "Fonte TCE"
+    "Previsão Inicial" as vl_previsao_inicial,
+    "Previsão Atualizada" as vl_previsao_atualizada,
+    "Arrecadada Mês" as vl_arrecadada_mes,
+    "Arrecadada Ano" as vl_arrecadada_ano,
+    "A Realizar" as vl_a_realizar,
+    "Natureza" as nm_natureza_receita,
+    nm_arquivo as nm_arquivo_origem,
+    "Fonte TCE" as nm_fonte_tce,
+    right(split_part(lower(nm_arquivo), '.', 1), 2) as nu_mes_referencia
 from {{ ref('stg_pjf_receita_mensal_comparativa') }}
